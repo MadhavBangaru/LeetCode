b9139f1be3892fdd1a450c2f6bdce775f68bec26
Madhav <bangarumadhav@gmail.com>
2023-04-30 23:23:19 +0530

    removed java related files

--- Programming_Skills/Day 1: Basic Data Types/1523. Count Odd Numbers in an Interval Range.java
+++
@@ -1,9 +0,0 @@
-package Day 1: Basic Data Types;
-//https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
-public class Solution {
-    public int countOdds(int low, int high) {
-        if(low % 2 != 0) low--;
-        if(high % 2 != 0) high++;
-        return (high-low)/2;
-    }
-}
