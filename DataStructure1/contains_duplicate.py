531e44bf72c735469d920ac756e23100f5900769
Madhav <bangarumadhav@gmail.com>
2023-04-30 22:40:44 +0530

    started with DS 1

---
+++ DataStructure1/contains_duplicate.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/contains-duplicate/?envType=study-plan&id=data-structure-i
+
+'''
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+'''
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+    
+
+solution = Solution()
+nums1 = [1,2,3,1]
+print(solution.containsDuplicate(nums1))
+
+nums2 = [1,2,3,4]
+print(solution.containsDuplicate(nums2))
+    
